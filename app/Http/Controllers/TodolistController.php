aecd37a082e659d5643abcd6e4e24e8f444e6cc3
haris2303 <hrsccf102@gmail.com>
2023-09-14 07:37:16 +0900

    Add Todolist Controller

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+    public function todoList()
+    {
+    }
+
+    public function addTodo()
+    {
+    }
+
+    public function removeTodo()
+    {
+    }
+}
