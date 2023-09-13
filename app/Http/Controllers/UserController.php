407f073205bcc70d7b6de5c027a984e13d591453
haris2303 <hrsccf102@gmail.com>
2023-09-13 21:02:53 +0900

    Create Login Action

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UserService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
-    public function login()
+
+    private UserService $userService;
+
+    public function __construct(UserService $userService)
     {
+        $this->userService = $userService;
     }
 
-    public function doLogin()
+    public function login(): Response
     {
+        return response()
+            ->view('user.login', [
+                'title' => 'Login Todolist'
+            ]);
+    }
+
+    public function doLogin(Request $request): Response|RedirectResponse
+    {
+        $user = $request->input('user');
+        $password = $request->input('password');
+
+        // validated input
+        if (empty($user) || empty($password)) {
+            return response()->view('user.login', [
+                'title' => 'Login Todolist',
+                'error' => 'User and password is required!'
+            ]);
+        }
+
+        if ($this->userService->login($user, $password)) {
+            $request->session()->put("user", $user);
+            return redirect('/');
+        }
+
+        return response()->view('user.login', [
+            'title' => 'Login Todolist',
+            'error' => 'User or password is wrong'
+        ]);
     }
 
     public function doLogout()
