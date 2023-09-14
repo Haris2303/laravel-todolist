9dcb122115f77c12916853c18e53a6454dafd320
haris2303 <hrsccf102@gmail.com>
2023-09-14 13:27:01 +0900

    Create Add Todolist Action

--- app/Http/Controllers/TodolistController.php
+++ app/Http/Controllers/TodolistController.php
@@ -2,16 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TodolistService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TodolistController extends Controller
 {
-    public function todoList()
+
+    private TodolistService $todolistService;
+
+    public function __construct(TodolistService $todolistService)
     {
+        $this->todolistService = $todolistService;
     }
 
-    public function addTodo()
+    public function todoList(): Response
     {
+        $todolist = $this->todolistService->getTodolist();
+
+        $data = [
+            "title" => "Todolist Page",
+            "todolist" => $todolist
+        ];
+
+        return response()->view('todolist.index', $data);
+    }
+
+    public function addTodo(Request $request): Response|RedirectResponse
+    {
+        $todo = $request->input('todo');
+
+        // check input empty
+        if (empty($todo)) {
+            $todolist = $this->todolistService->getTodolist();
+            return response()->view('todolist.index', [
+                'title' => 'Todolist Page',
+                'todolist' => $todolist,
+                'error' => 'Todo is required'
+            ]);
+        }
+
+        // save todolist
+        $this->todolistService->saveTodo(uniqid(), $todo);
+
+        return redirect()->action([TodolistController::class, 'todoList']);
     }
 
     public function removeTodo()
---
+++ tests/Feature/TodolistControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodolistControllerTest extends TestCase
+{
+    public function testTodolistPage()
+    {
+        $this->withSession([
+            'user' => 'udinsurudin'
+        ])->get('/todolist')->assertSeeText('Sign Out');
+    }
+
+    public function testTodolistFailed()
+    {
+        $this->withSession([
+            'user' => 'udinsurudin'
+        ])->post('/todolist', [])->assertSeeText('Todo is required');
+    }
+
+    public function testTodolistSuccess()
+    {
+        $this->withSession([
+            'user' => 'udinsurudin'
+        ])->post('/todolist', [
+            'id' => '1',
+            'todo' => 'Memasak'
+        ])->assertRedirect('/todolist');
+    }
+}
